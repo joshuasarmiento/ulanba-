c955212df99caa1da6dde5901b74c7a6b51ad99f
jadmin <supremojoshua22@gmail.com>
2025-07-22 11:54:41 +0800

    Gradient text for ios

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,7 @@
     <header class="text-gray-800 p-4 flex items-center justify-center pt-8">
         <img src="../assets/haze_animated.svg" class="h-12 w-12" alt="Logo" />
         <h1
-            class="text-2xl md:text-3xl font-bold bg-gradient-to-r from-amber-500 to-blue-500 bg-clip-text text-transparent">
+            class="gradient-text text-2xl md:text-3xl font-bold bg-gradient-to-r from-amber-500 to-blue-500 bg-clip-text text-transparent">
             Ulan Ba?</h1>
     </header>
 </template>
@@ -13,4 +13,7 @@
 
 <style scoped>
 /* Add any specific styles for the header here if needed */
+.gradient-text {
+    -webkit-box-decoration-break: clone;
+} 
 </style>
