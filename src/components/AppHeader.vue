8607b12dc6d991154a92bbf4ef9b7c26b028e0c2
jadmin <supremojoshua22@gmail.com>
2025-08-04 13:21:56 +0800

    update

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,7 +4,7 @@
             <img src="../assets/haze_animated.svg" class="h-auto w-16 md:w-20" alt="Logo" />
             <h1
                 class="gradient-text text-4xl py-2 text-[#CD9E73] md:text-[2rem] font-bold md:bg-gradient-to-r md:from-[#CD9E73] md:to-[#56A0EE] md:bg-clip-text md:text-transparent">
-                Ulan Ba Dyan?</h1>
+                UlanBaDyan?</h1>
         </div>
     </header>
 </template>
